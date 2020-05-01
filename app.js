892ac2e87e87ae8a9bd3b40aea2df4dd07f583b6
Zorayda <zgutierrez@americanassist.com>
2020-05-01 16:03:03 -0500

    Cambios en colores y se colocan métodos para dinamizar las acciones del usuario en conjunto con la interfaz visual

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ new Vue({
              name: 'Bitcoin',
              img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
              changePercent: -10,
+             priceCurrent: 8400,
              prices:  [8400, 7900, 8200, 9000, 9400, 10000, 10200],
              pricesWithDays:  [
                 { day: 'Lunes', value: 8400 },
@@ -15,8 +16,17 @@ new Vue({
                 { day: 'Viernes', value: 9400 },
                 { day: 'Sábado', value: 10000 },
                 { day: 'Domingo', value: 10200 },
-             ]
+             ],
+             showPrices: false,
+             color: 'f4f4f4'
         }
+    },
+
+    methods: {
+      toggleShowPrices(){
+        this.showPrices = !this.showPrices,
+        this.color = this.color.split('').reverse().join('')
+      }
     }
 })
 
