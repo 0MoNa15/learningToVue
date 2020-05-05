e59c17ef281b023999e5b51fda7a387af66a1484
Zorayda <zgutierrez@americanassist.com>
2020-05-04 21:33:59 -0500

    Creación de carpeta para producción

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Exchange v1</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Que permite el uso de sus características básicas. Es funcional para
+      obtener las criptomoneditas actuales.
+      <br />Proyecto de aprendizaje de Vue.
+    </p>
+
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la página de inicio
+    </router-link>
+  </div>
+</template>
